a67504778fe51b0e88eb26b7622a03e9fb3c2d81
Dave Hylands <dhylands@gmail.com>
2024-02-26 08:03:40 -0800

    Added DuinoLog.h in the root

---
+++ DuinoLog.h
@@ -0,0 +1,24 @@
+/****************************************************************************
+ *
+ *   @copyright Copyright (c) 2024 Dave Hylands     <dhylands@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the MIT License version as described in the
+ *   LICENSE file in the root of this repository.
+ *
+ ****************************************************************************/
+/**
+ *   @file   DuinoLog.h
+ *
+ *   @brief  Logging and string manipulation functions.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include "src/ArduinoColorSerialLog.h"
+#include "src/ArduinoSerialLog.h"
+#include "src/DumpMem.h"
+#include "src/Log.h"
+#include "src/Str.h"
+#include "src/Util.h"
